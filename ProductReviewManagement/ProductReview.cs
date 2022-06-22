671ece574657956fbc303d1e13e35784300c7cfb
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-06-22 19:24:59 +0530

    [jayesh] Reterive the userId

--- ProductReviewManagement/ProductReview.cs
+++ ProductReviewManagement/ProductReview.cs
@@ -155,6 +155,19 @@ namespace ProductReviewManagement
             }
             return nameList;
         }
+        //UC 12: Retrieve Record based on Rating where userid=10 
+        public string RetrieveRecordsBasedOnRating()
+        {
+            CreateDataTable();
+            string nameList = "";
+            var res = (from product in productdt.AsEnumerable() where product.Field<Int32>("UserId") == 10 orderby product.Field<int>("Rating") select product).ToList();
+            foreach (var p in res)
+            {
+                Console.WriteLine("{0} | {1} | {2} | {3} | {4} ", p["ProductId"], p["UserId"], p["Rating"], p["Review"], p["IsLike"]);
+                nameList += p["Rating"] + " ";
+            }
+            return nameList;
+        }
         public void DisplayList()
         {
             foreach (Product product in ProductList)
